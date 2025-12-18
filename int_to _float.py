b070503a5b0462eb4841aa39f722f8d4460fc318
poojithamotha <47564996+poojithamotha@users.noreply.github.com>
2025-12-18 10:20:39 +0530

    Add files via upload

---
+++ int_to _float.py
@@ -0,0 +1,12 @@
+print("Read an integer and convert it to float. Print both")
+# by default python will read the i/p as string, so using the int() method
+num1 = int(input("Enter any integer here to convert it into float: "))
+#converting into float
+num2 = float(num1)
+print("printing both values")
+print(num1)
+# to check the type of i/p
+print(type(num1))
+print(num2)
+# to check the type after conversion
+print(type(num2))
