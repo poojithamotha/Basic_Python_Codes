5b684e4721553526ba9dfef9fdf1d019bc3e4222
poojithamotha <47564996+poojithamotha@users.noreply.github.com>
2025-12-18 10:27:10 +0530

    Add files via upload

---
+++ float_to_int.py
@@ -0,0 +1,10 @@
+print("Read a float number and convert it to integer. Print the result.")
+# by default python will read the i/p as string, so using the float() method
+num1 = float(input("Enter any float here to convert it into integer: "))
+#example float = 3.5 it will be converted to 3 as an integer
+#converting into integer
+num2 = int(num1)
+#printing the int number
+print(num2)
+#printing the type of it
+print(type(num2))
