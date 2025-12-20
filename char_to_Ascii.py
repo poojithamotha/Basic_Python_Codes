f7ffdc9cd02b1d57779140753b72fd1f31815cbe
poojithamotha <47564996+poojithamotha@users.noreply.github.com>
2025-12-20 10:57:26 +0530

    Add files via upload

---
+++ char_to_Ascii.py
@@ -0,0 +1,7 @@
+print("Read a character and print its ASCII value.")
+# reading the input character as string
+char = input("Enter any character will print its ASCII value: ")
+# finding its Ascii value using built in function ord()
+Ascii_val = ord(char)
+# printing its Ascii value
+print(Ascii_val)
